408303a3550183d7b0fb3df7a5753460b591f985
hamza507 <65030926+hamza507@users.noreply.github.com>
2020-06-14 10:23:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+// Chapter # 1 
+
+// alert ("Welcome to hamza Website");
+
+// alert ("Error! Please enter a valid password");
+
+// alert ("Welcome to Javascript Land Happy Coding");
+
+// lert ("Welcome to Javascript Land");
+// alert ("Prevent this page from creating additional dialogues");
+
+// console.log ("Hello....I can run JS through my web browsers console");
+
+
+// Chapter # 2
+
+// alert("User name");
+
+// var username ="hamza Imtiaz";
+// alert (username);
+
+// var message="Hello world";
+// alert (message);
+
+
+// var name="Suleman Imtiaz"
+// alert(name)
+// var age ="18 years old"
+// alert (age)
+// var work ="Certified Mobile Application Development"
+// alert (work)
+
+
+
+// Chapter 3
+
+
+// var age="I am 18 Years old"
+// alert (age);
+
+
+// var a=1; 
+// var b=a++; 
+// alert(b); 
+
+
+// var birthyear= "My Birth Year is 2002"; 
+// document.write(birthyear);
+
+
+
+// Chapter 4 
+
+
+// var person = "John Doe", carName = "Volvo", price = 200;
+
+
+// var display= "Rules for naming JS Variables"
+// document.write(display);
+
+
+
+
+// Chapter 5 
+
+// var a=5; 
+// var b=10; 
+// var c= a+b; 
+// document.write(c);
+
+
+
+// var a=15; 
+// var b=10; 
+// var c= a-b; 
+// document.write(c);
+
+
